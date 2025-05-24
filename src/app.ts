ab7daae72926adb880d047833fbe8c75ec4b1b1d
Nicolò Pincelli <pincelli.n@gmail.com>
2025-05-24 12:49:55 +0200

    feat: create first class Project input

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
 // Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  containerElement: HTMLDivElement;
+  element: HTMLFormElement;
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.containerElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+    this.attach();
+  }
+
+  private attach() {
+    this.containerElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const formProject = new ProjectInput();
