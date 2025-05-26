2ba62c572a069ac2831e117db1830eb06eb25617
Nicolò Pincelli <pincelli.n@gmail.com>
2025-05-26 15:00:39 +0200

    feat: project list classs

--- src/app.ts
+++ src/app.ts
@@ -46,6 +46,42 @@ function autobind(_: any, _2: String, descriptor: PropertyDescriptor) {
   return adjDescriptor;
 }
 
+//ProjectList
+
+class ProjectList {
+  templateElemente: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElemente = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElemente.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-project-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + "PROJECTS";
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
+//ProjectInput
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   containerElement: HTMLDivElement;
@@ -138,3 +174,5 @@ class ProjectInput {
 }
 
 const formProject = new ProjectInput();
+const acriveProjectList = new ProjectList("active");
+const finishedProjectList = new ProjectList("finished");
