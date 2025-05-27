0222f6f263776297952fdd981cb6f73cbc228258
Nicolò Pincelli <pincelli.n@gmail.com>
2025-05-27 17:00:48 +0200

    add: comment

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 ///<reference path='components/project-list.ts'/>
 ///<reference path='components/project-input.ts'/>
 
+/*
+Nelle versioni più moderne di TypeScript (o JavaScript con moduli), i componenti o le funzioni vengono importati tramite:
+import { nomeFunzione } from 'percorso-del-modulo';
+
+Questo sostituisce l'uso delle triple slash directive (/// <reference />), che era comune nei progetti precedenti.
+*/
 namespace App {
   new ProjectInput();
   new ProjectList("active");
